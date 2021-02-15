844245222beec07f3863c5c50c14a4715a6c35c4
matteo <matteo@matteos-MacBook-Pro.local>
2021-02-15 00:25:15 -0800

    Added random image generator

--- generator.js
+++ generator.js
@@ -1,13 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('form').on('submit', function(event) {
+    let subject;
+    let image;
+
+    $('form').on('submit', function (event) {
+        let container = $(this).parent();
+        console.log(container)
         event.preventDefault();
+        subject = $(this).find('input[type = text]');
+        subject = subject.val();
+
+
+        image = $(this).next().find("img[alt='Randomly Generated Image']");
+
+        if (subject != '') {
+            image.attr('src', 'https://source.unsplash.com/400x250/?' + subject);
+        } else {
+            console.log('err')
+        }
 
     })
 
-    
-
-    if (subject != '') {
-        image.attr('src', 'https://source.unsplash.com/400x250/' + subject);
-    }
 })
