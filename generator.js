c77aa3485101561042daeebea8ee06fcb60d4fe4
matteo <matteo@matteos-MacBook-Pro.local>
2021-02-15 01:03:41 -0800

    finsihed required features

--- generator.js
+++ generator.js
@@ -4,21 +4,25 @@ $(document).ready(function () {
     let image;
 
     $('form').on('submit', function (event) {
-        let container = $(this).parent();
-        console.log(container)
+
         event.preventDefault();
         subject = $(this).find('input[type = text]');
         subject = subject.val();
 
-
         image = $(this).next().find("img[alt='Randomly Generated Image']");
 
         if (subject != '') {
-            image.attr('src', 'https://source.unsplash.com/400x250/?' + subject);
+            image = image.attr('src', 'https://source.unsplash.com/400x250/?' + subject);
         } else {
-            console.log('err')
+            console.log('err');
         }
+    })
 
+    $('form').find("button[type='button']").on('click', function (event) {
+        let form = $(this).parent();
+        let img = form.next().find("img[alt='Randomly Generated Image']");
+
+        $(img).clone().appendTo('#image-gallery');    
     })
 
 })
